ceab5deb05f49a51b7c41f333865f537cabbb3c5
alejo <alejoatria@gmail.com>
2022-07-12 09:41:48 -0400

    copiado code anterior. commit inicial nuevo proyecto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import List from "./list.js";
+import Footer from "./footer.js";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+let hardcodedtask = ['brush teeth', 'wash hands', 'walk the dog', 'feed the cats'];
 
-//create your first component
 const Home = () => {
+	const [task, setTask] = useState(hardcodedtask);
+
+	//ADD TODO
+	let handlesubmit = (e) => {
+		if (e.key === 'Enter') {
+			let auxarr = [...task];
+			auxarr.push(e.target.value);
+			setTask(auxarr);
+			e.target.value = '';
+		}
+	}
+
+	//DELETE TODO
+	let deletetodo = (index) => {
+		let auxarr = [...task];
+		auxarr[index] = null;
+		let filtered = auxarr.filter((e) => e !== null);
+		setTask(filtered);
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex justify-content-center">
+			<ul className="list-group col-md-6 mt-5">
+				<li className="list-group-item">
+					<input className="form-control" type="text" placeholder="What needs to be done?" onKeyDown={(e) => handlesubmit(e)} />
+				</li>
+				<List task={task} deletetodo={deletetodo} />
+				<Footer task={task} />
+			</ul>
 		</div>
 	);
 };
